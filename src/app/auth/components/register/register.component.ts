af4085640fb6f0c5d67c535bb3d892a7c0eb8b29
Diego Maggessi <diegosmteixeira@gmail.com>
2024-08-09 14:39:02 -0300

    add: register component

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,29 @@
+import {Component} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+@Component({
+  selector: 'mc-register',
+  templateUrl: './register.component.html',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+})
+export class RegisterComponent {
+  form: FormGroup;
+
+  onSubmit() {
+    console.log('form', this.form.getRawValue());
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.nonNullable.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+}
